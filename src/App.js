12f075a225f39e9cce90826174b0edda06f43492
jochun <jkchun@protonmail.com>
2022-03-14 23:40:35 -1000

    about and nav components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// this file is the center of the application, it is the root component that houses all other components
+// need to import React in evey component file
+import React from 'react';
+// import any images or CSS you want
+// import './App.css';
+
+import Nav from './components/Nav';
+import About from './components/About';
+
 
 function App() {
+  // this function returns JSX: a language that can represent HTML in JS
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
