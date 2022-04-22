06c9d33efc4139ae6916535f500f5ecab06cf531
jochun <jkchun@protonmail.com>
2022-04-21 19:35:59 -1000

    fixing bug in components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-// this file is the center of the application, it is the root component that houses all other components
-// need to import React in evey component file
 import React from 'react';
-// import any images or CSS you want
-// import './App.css';
-
 import Nav from './components/Nav';
 import About from './components/About';
 
-
 function App() {
-  // this function returns JSX: a language that can represent HTML in JS
+
   return (
-    <div >
-      <Nav></Nav>
+    <div>
+      <Nav />
       <main>
-        <About></About>
+          <About />        
       </main>
     </div>
   );
